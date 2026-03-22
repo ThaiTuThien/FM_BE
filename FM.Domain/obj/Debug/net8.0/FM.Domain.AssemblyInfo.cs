4021fcddae9fa5818e12e3c0b8bd50ba3480b7f1
Omega1831 <lythanhphong741@gmail.com>
2026-03-22 23:44:44 +0700

    Phong: Hoan thanh API Product voi cau truc Clean Architecture

--- FM.Domain/obj/Debug/net8.0/FM.Domain.AssemblyInfo.cs
+++ FM.Domain/obj/Debug/net8.0/FM.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FM.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5eba187a22cbd9203861f69f0de1852f699a40d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("FM.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FM.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
